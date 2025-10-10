c07c886b13a859db266caee950d4fab5eb6326e2
galasik_stanislav <galasik_stanislav@icloud.com>
2025-10-10 23:44:16 +0300

    fixed name of  class

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,20 @@
 const burgerBtn = document.querySelector('.burger-icon');
-const menu = document.querySelector('.modal');
-const closeBtn = document.querySelector('.modal-nav-close');
+const menu = document.querySelector('.header-modal');
+const closeBtn = document.querySelector('.header-modal-nav-close');
 
 burgerBtn.addEventListener('click', () => {
   menu.classList.add('is-open');
+  body.classList.add('body--no-scroll');
 });
 
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('is-open');
+  body.classList.remove('body--no-scroll');
 });
 const menuLinks = menu.querySelectorAll('a');
 menuLinks.forEach(link => {
   link.addEventListener('click', () => {
     menu.classList.remove('is-open');
+    body.classList.remove('body--no-scroll');
   });
 });
