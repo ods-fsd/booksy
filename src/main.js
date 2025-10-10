20dab37de0cd621ac59876fd4f551a1d3f8c7e4e
Oleksiy_Lishchuk <ejik.alex9294@gmail.com>
2025-10-10 12:28:42 +0300

    add import to main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/books.js';
+import './js/feedback.js';
+import './js/events.js';
+import './js/footer.js';
+import './js/button-up.js';
+import './js/modal.js';
