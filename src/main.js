85bbe54be423bded566e2999339597c195218270
Denys Orlyk <idsorlyk@gmail.com>
2025-10-11 17:54:55 +0200

    fix: correct import path for feedbacks module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/header.js';
 import './js/hero.js';
 import './js/books.js';
-import './js/feedback.js';
+import './js/feedbacks.js';
 import './js/events.js';
 import './js/footer.js';
 import './js/button-up.js';
